670c9d85c71d9a2e1591b2f4c4c4456d2e3c42eb
CHROSW <oschristophe@gmail.com>
2023-08-11 18:21:41 +0200

    Ajout de script avec jquery dans functions.php pour l'animation de la section histoire et modification de l'animation pour cette section

--- foce-child/functions.php
+++ foce-child/functions.php
@@ -3,7 +3,9 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css',array('parent-style'), false, '0.1', 'all');
-    wp_enqueue_script( 'my-script', get_stylesheet_directory_uri() . '/js/script.js');
+    wp_register_script( 'my-script', get_stylesheet_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('my-script');
 }
 
 
