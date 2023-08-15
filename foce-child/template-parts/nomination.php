dbc52eba2c954dbfa6d7864c7c6d9f3045ade9c5
CHROSW <oschristophe@gmail.com>
2023-08-15 16:01:10 +0200

    Ajout de la section nomination aux oscars 2022

---
+++ foce-child/template-parts/nomination.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<section id="festival">
+    <article>
+        <div class="nomination">
+            <p>Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !<p>
+        </div>
+        <img src="/koukaki/wp-content/themes/foce-child/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021 1.png" alt="nomination aux oscars 2022"/>
+    </article>   
+</section>
